e6a33fa553eb8061b3871d1b76dfc74033f83fc4
Wallace Turner <wallaceturner@gmail.com>
2019-09-12 23:26:25 +0800

    script

--- desktop/install.ps1
+++ desktop/install.ps1
@@ -1,8 +1,11 @@
 #./vs_community__217969693.1566954722.exe /q
 
-#https://www.fosshub.com/ConEmu.html?dwl=ConEmuSetup.190714.exe 
+
+$apps = "F:\software\desktop"
+Invoke-WebRequest -Uri https://www.fosshub.com/ConEmu.html?dwl=ConEmuSetup.190714.exe  -OutFile ./ConEmuSetup.190714.exe
 ./ConEmuSetup.190714.exe /p:x64 /q
-$apps = "F:\software\verified"
+
+Invoke-WebRequest -Uri https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.msi -OutFile $apps/vagrant_2.2.5_x86_64.msi
 
 & "$apps\flux-setup.exe" /S
 

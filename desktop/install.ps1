5a83a4071dbd6444076ee462316c880675f3afc6
wallace_turner <wallaceturner@gmail.com>
2019-09-16 12:21:35 +0800

    t1

--- desktop/install.ps1
+++ desktop/install.ps1
@@ -9,27 +9,15 @@ choco install telegram -y
 choco install 7zip processhacker -y
 choco install google-backup-and-sync -y
 choco install obs-studio -y
-choco install mongodb studio3t sql-server-management-studio mremoteng
+choco install mongodb studio3t sql-server-management-studio mremoteng conemu f.lux resharper
 
-#./vs_community__217969693.1566954722.exe /q
+F:\software\desktop\MSBuild.Community.Tasks.v1.5.0.235.msi
+
+#required for FexOnline sln
+F:\software\desktop\PC-Span-x64593.msi
+F:\software\desktop\SpanCom593-x64.msi
+
 Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
-
-cd "F:\software\desktop"
-
-& "./ConEmuSetup.190714.exe" /p:x64 /q
-
-Invoke-WebRequest -Uri https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.msi -OutFile $apps/vagrant_2.2.5_x86_64.msi
-
-& "./flux-setup.exe" /S
-
-& "./VirtualBox-6.0.10-132072-Win.exe" --silent
-
-./SSMS-Setup-ENU.exe /S
 
 #Invoke-WebRequest -Uri https://us.download.nvidia.com/GFE/GFEClient/3.20.0.118/GeForce_Experience_v3.20.0.118.exe -OutFile ./GeForce_Experience_v3.20.0.118.exe
 
-#msiexec /qn /i C:\temp\vagrant_2.2.5_x86_64.msi
-
-msiexec /i "$./tortoisehg-5.0.2-x64.msi" /quiet /qn /norestart
-
-msiexec /qn /l* node-log.txt /i node-v10.16.3-x64.msi

67af19671747910d16726b2721e386286b2a3a64
wallace_turner <wallaceturner@gmail.com>
2019-09-21 13:15:22 +0800

    disable shutdown reason

--- windows_server/install.ps1
+++ windows_server/install.ps1
@@ -15,37 +15,38 @@ param (
 
 if ($step -eq 1){
 	 Write-Host("Step 1");
-	 # bcdedit /set hypervisorlaunchtype off
-	# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	bcdedit /set hypervisorlaunchtype off
+	Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-	# Set-ExecutionPolicy RemoteSigned -f
+	Set-ExecutionPolicy RemoteSigned -f
 
-	# choco feature enable -n allowGlobalConfirmation
-	# choco install tortoisehg vcredist140 googlechrome mremoteng notepadplusplus autologon
+	choco feature enable -n allowGlobalConfirmation
+	choco install tortoisehg vcredist140 googlechrome mremoteng notepadplusplus autologon
 
-	# autologon $env:UserName $env:UserDomain vagrant
-	# dism /online /Enable-Feature /FeatureName:WirelessNetworking /NoRestart
-	# DISM /online /Enable-Feature /FeatureName:TelnetClient /NoRestart
-	# DISM /Online /Enable-Feature /FeatureName:NetFx3 /All /NoRestart
+	autologon $env:UserName $env:UserDomain vagrant
+	dism /online /Enable-Feature /FeatureName:WirelessNetworking /NoRestart
+	DISM /online /Enable-Feature /FeatureName:TelnetClient /NoRestart
+	DISM /Online /Enable-Feature /FeatureName:NetFx3 /All /NoRestart	
+	New-Item 'HKLM:\Software\Policies\Microsoft\Windows NT\Reliability' -Force | New-ItemProperty -Name ShutdownReasonOn -Value 0 -Force | Out-Null	
 }
 elseif ($step -eq 2){
-	 Write-Host("Step 2");
-	 ipconfig
+	Write-Host("Step 2");	 
 	 
-	 # Get-ChildItem -Path C:\software\verified\
+	Get-ChildItem -Path C:\software\verified\
+	cd C:\software\verified\
+	#install pulse
+	msiexec /i ps-pulse-win-9.0r4.0-b1731-64bitinstaller.msi /quiet /qn
 
-	# cd C:\software\verified\
-	# #install pulse
-	# msiexec /i ps-pulse-win-9.0r4.0-b1731-64bitinstaller.msi /quiet /qn
+	#install SQL server
+	copy C:\software\en_sql_server_2014_developer_edition_with_service_pack_2_x64_dvd_8967821.iso c:\sqlsvr.iso
+	Mount-DiskImage C:\sqlsvr.iso -PassThru
+	d:\setup.exe /ConfigurationFile=C:\vagrant\install_files\ConfigurationFile.ini
+	Dismount-DiskImage -ImagePath C:\sqlsvr.iso
+	Remove-Item C:\sqlsvr.iso
 
-	# #install SQL server
-	# copy C:\software\en_sql_server_2014_developer_edition_with_service_pack_2_x64_dvd_8967821.iso c:\sqlsvr.iso
-	# Mount-DiskImage C:\sqlsvr.iso -PassThru
-	# d:\setup.exe /ConfigurationFile=C:\vagrant\install_files\ConfigurationFile.ini
-	# Dismount-DiskImage -ImagePath C:\sqlsvr.iso
-	# Remove-Item C:\sqlsvr.iso
-
-	# New-NetFirewallRule -DisplayName '1433' -Profile 'Any' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 1433
+	New-NetFirewallRule -DisplayName '1433' -Profile 'Any' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 1433
+	
+	ipconfig
 }
 
 

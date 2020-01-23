d0a0ba8c86195b7bbe46c796ca4262a5bce4274d
Wallace Turner <wallaceturner@gmail.com>
2020-01-23 08:29:43 +0800

    monero scripts

---
+++ monero/install.sh
@@ -0,0 +1,7 @@
+#to be installed on ubuntu server 
+#sudo vi /etc/systemd/system/monerod.service
+sudo apt  install ncdu
+#tail -f .bitmonero/bitmonero.log
+sudo apt install network-manager
+sudo service network-manager start
+#use nmtui to connect to wifi

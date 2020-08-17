51f8272301594c8f1ac5438a4baae29d6743d966
wallace_turner <wallaceturner@gmail.com>
2020-08-17 22:44:33 +0800

    docker mssql

---
+++ ubuntu/install_files/docker_mssql.sh
@@ -0,0 +1,8 @@
+docker run --name mssql -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=P@ssword1' -p 1433:1433 -d -v sqldata:/var/opt/mssql mcr.microsoft.com/mssql/server:2017-latest 
+docker exec -it mssql mkdir /var/opt/mssql/backup
+docker cp shared/backup.bak mssql:/var/opt/mssql/backup
+
+#
+docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "P@ssword1" -Q "RESTORE FILELISTONLY FROM DISK = '/var/opt/mssql/backup/backup.bak'"
+
+docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'P@ssword1' -Q 'RESTORE DATABASE MercariDirectNightly FROM DISK = "/var/opt/mssql/backup/backup.bak" WITH MOVE "MercariDirectNightly" TO "/var/opt/mssql/data/MercariDirectNightly.mdf", MOVE "MercariDirectNightly_log" TO "/var/opt/mssql/data/MercariDirectNightly.ldf"'

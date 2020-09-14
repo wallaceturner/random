b015ff72be402e444e23b00a2f5df04e27d09ef5
wallace_turner <wallaceturner@gmail.com>
2020-09-14 09:48:54 +0800

    restore staging

--- ubuntu/install_files/docker_mssql.sh
+++ ubuntu/install_files/docker_mssql.sh
@@ -6,3 +6,6 @@ docker cp shared/backup.bak mssql:/var/opt/mssql/backup
 docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "P@ssword1" -Q "RESTORE FILELISTONLY FROM DISK = '/var/opt/mssql/backup/backup.bak'"
 
 docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'P@ssword1' -Q 'RESTORE DATABASE MercariDirectNightly FROM DISK = "/var/opt/mssql/backup/backup.bak" WITH MOVE "MercariDirectNightly" TO "/var/opt/mssql/data/MercariDirectNightly.mdf", MOVE "MercariDirectNightly_log" TO "/var/opt/mssql/data/MercariDirectNightly.ldf"'
+
+#staging
+docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'P@ssword1' -Q 'RESTORE DATABASE MercariDirectStaging FROM DISK = "/var/opt/mssql/backup/staging.bak" WITH MOVE "MercariDirectNightly" TO "/var/opt/mssql/data/MercariDirectStaging.mdf", MOVE "MercariDirectNightly_log" TO "/var/opt/mssql/data/MercariDirectStaging.ldf"'
